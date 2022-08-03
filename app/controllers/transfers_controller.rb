e0230a2dac6d449d10001264c8cd521731b0be2d
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-03 10:07:30 -0300

    feat(Transfer): add crud.

---
+++ app/controllers/transfers_controller.rb
@@ -0,0 +1,56 @@
+class TransfersController < ApplicationController
+  before_action :set_transfer, only: %i[ show update destroy ]
+
+  def index
+    @transfers = transfer_status
+    render(json: @transfers)
+  end
+
+  def show
+    render(json: @transfer)
+  end
+
+  def create
+    @transfer = Transfer.new(transfer_params)
+
+    if @transfer.save
+      render(json: @transfer, status: :created)
+    else
+      render(json: @transfer.errors, status: :unprocessable_entity)
+    end
+  end
+
+  def update
+    if @transfer.update(transfer_params)
+      render json: @transfer
+    else
+      render json: @transfer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @transfer.destroy
+  end
+
+  private
+    def set_transfer
+      @transfer = Transfer.find(params[:id])
+    end
+
+    def transfer_params
+      params.require(:transfer).permit(:recipient_account, :balance)
+    end
+
+    def transfer_status
+      @status = params["status"]
+      if (@status == "sent") 
+        return Transfer.sent
+
+      elsif (@status == "received")
+        return Transfer.received
+
+      else
+        return Transfer.all
+      end
+    end
+end
