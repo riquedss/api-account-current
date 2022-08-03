d52a5b2d0a861922bc783aeeff2270b2f7817a87
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-03 17:48:09 -0300

    feat(auth): add função de login para user.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -10,11 +10,22 @@ class AuthController < ApplicationController
   end
 
   def login
+    @user = User.find_by(cpf: login_params[:cpf])
+    if @user&&@user.authenticate(login_params[:password])
+      token = JsonWebToken::Base.encode(user_id: @user.id)
+      render(json: { user: @user, token: token },  status: :ok)
+    else
+      render(json: { message: "Cpf or Password incorrect" }, status: :unauthorized)
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :last_name, :email, :cpf, :password, :password_confirmation)
+  end
+
+  def login_params
+    params.require(:user).permit(:cpf, :password)
   end
 end
