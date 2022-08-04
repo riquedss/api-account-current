9a20a3068f9971d5cc388b2f5a1e8d085b715bbb
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-04 04:25:49 -0300

    feat(user): add mais validações.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
 
     validates :name, :last_name, :email, :cpf, presence: true
     validates :name, :last_name, format: { with: REGEX_LETRA }
-    validates :email, format: { with: REGEX_EMAIL }
-    validates :cpf, format: { with: REGEX_CPF }
+    validates :email, uniqueness: { case_sensitive: false }, format: { with: REGEX_EMAIL }
+    validates :cpf, uniqueness: true, format: { with: REGEX_CPF }
     validates :password, length: { is: 6 }
 
     enum role: { comum: 0, vip: 1, manager: 2 }
