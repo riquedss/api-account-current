611353c9ccce6fbaa7896a72367ada5e91051cc8
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-03 23:23:58 -0300

    feat(auth_accounts_controller): controller para test.

---
+++ test/controllers/auth_accounts_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class AuthAccountsControllerTest < ActionDispatch::IntegrationTest
+  test "should get signup" do
+    get auth_accounts_signup_url
+    assert_response :success
+  end
+
+  test "should get login" do
+    get auth_accounts_login_url
+    assert_response :success
+  end
+end
