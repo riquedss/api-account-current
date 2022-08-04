7be360a88e01229b7703590a4f19e35c4cf4cda1
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-04 04:20:35 -0300

    chore(db): set manager in db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Henrique", 
+            last_name: "gerente", 
+            email: "teste2@teste.com", 
+            cpf: "10476467571",
+            role: 2,
+            password: "123456",
+            password_confirmation: "123456")
