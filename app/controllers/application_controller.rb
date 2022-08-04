ca39b591f2f9bb04cd3f55963c264df615413e82
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-03 23:28:57 -0300

    feat(auth): add funções de autenticação de conta e usuário.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+    def current_user     
+        return nil if !token || !decoded_payload
+        User.find_by(id: decoded_payload[0]["user_id"])
+    end
+
+    def verify_authenticated_user
+        if !current_user
+            render(json: { message: "You aren't authenticated." }, status: 401)
+        end
+    end
+
+    def current_checking_account
+        return nil if !token || !decoded_payload
+        Checking_account.find_by(id: decoded_payload[0]["checking_account_id"])
+    end
+
+    def verify_authenticated_checking_account
+        if !current_checking_account
+            render(json: { message: "You aren't authenticated." }, status: 401)
+        end
+    end
+
+    def token
+        request.headers["token"]
+    end
+
+    def decoded_payload
+        JsonWebToken::Base.decode(token)
+    end
 end
