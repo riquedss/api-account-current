ddd0119d37466c768b8c3b9a71f2d005f5515eaf
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-05 19:10:58 -0300

    feat: add validações para depósito e saque.

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,6 +2,26 @@ class Operation < ApplicationRecord
     belongs_to :checking_account
 
     validates :balance, :status, presence: true
+    validate :positive
+    validate :withdrawal_is_valid, if: :status_withdrawa_role_user_comum
     
     enum status: { deposit: 0, withdraw: 1 }
+
+    def status_withdrawa_role_user_comum
+      @checking_account = CheckingAccount.find(self.checking_account_id)
+      @user = User.find(@checking_account.user_id)
+      return self.withdraw? && @user.comum? 
+    end
+
+    def withdrawal_is_valid
+      if !(self.balance <= @checking_account.balance)
+        errors.add(:balance, "insufficient balance for this withdrawal")
+      end       
+    end
+    
+    def positive
+        if self.balance <= 0
+            errors.add(:balance, "balance is not positive")
+        end
+    end
 end
