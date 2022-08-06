62f659814e2f9997432f877bc83335abb15b34fe
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-06 18:30:32 -0300

    fix(user_comum_and_sent): correção na lógica da função para evitar class:nil.

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,34 +1,38 @@
 class Transfer < ApplicationRecord
-    belongs_to :checking_account
+  belongs_to :checking_account
 
-    validates :recipient_account, :balance, presence: true
-    validates :recipient_account, format: { with: REGEX_NUMBER }, length: { is: 5}
-    validate :positive, :recipient_account_exist, :recipient_account_different_sender
-    validate :limite, if: :user_comum
+  validates :transfer_account, :balance, presence: true
+  validates :transfer_account, format: { with: REGEX_NUMBER }, length: { is: 5}
+  validate :positive, :transfer_account_exist, :transfer_account_different_sender, if: :sent
+  validate :limite, if: :user_comum_and_sent
 
-    enum status: { sent: 0, received: 1 }
+  enum status: { sent: 0, received: 1 }
 
-    def recipient_account_exist
-      @checking_account = CheckingAccount.find_by(account: self.recipient_account )
-      if !@checking_account
-        errors.add(:recipient_account, "account does not exist")
-      end
+  def transfer_account_exist
+    @checking_account_received = CheckingAccount.find_by(account: self.transfer_account )
+    if !@checking_account_received
+      errors.add(:transfer_account, "account does not exist")
     end
+  end
 
-    def recipient_account_different_sender
+  def transfer_account_different_sender
     @checking_account = CheckingAccount.find(self.checking_account_id)
-      if @checking_account.account == self.recipient_account
-        errors.add(:recipient_account, "invalid transfer")
-      end
+    if @checking_account.account == self.transfer_account
+      errors.add(:transfer_account, "invalid transfer")
     end
+  end
 
-    def limite
-      if (self.balance > 1000) && ((self.balance + 8) > @checking_account.balance)
-        errors.add(:balance, "only vip can transfer more than 1000 and get a negative balance")
-      end
+  def limite
+    if (self.balance > 1000) || ((self.balance + 8) > @checking_account.balance)
+      errors.add(:balance, "only vip can have a negative balance or withdraw more than 1000")
     end
+  end
 
-    def user_comum
-        user(@checking_account).comum?
-    end
+  def user_comum_and_sent
+    self.sent? && user(@checking_account.user_id).comum? 
+  end
+
+  def sent
+    self.sent?
+  end
 end
