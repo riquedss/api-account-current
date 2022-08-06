825cd6e5246cb6c215a9ffa7ad79c2d42bb54f64
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-06 18:39:18 -0300

    feat: add nova regra de criação de transferência.

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -12,20 +12,26 @@ class TransfersController < ApplicationController
   end
 
   def create
-    @transfer = Transfer.new(transfer_params_with_account)
-
-    if @transfer.save
-      render(json: @transfer, status: :created)
-    else
-      render(json: @transfer.errors, status: :unprocessable_entity)
+    @transfer_sent = Transfer.new(transfer_params_with_account)
+    if !@transfer_sent.save 
+      return render(json: @transfer_sent.errors, status: :unprocessable_entity)
     end
+    
+    @transfer_received = Transfer.new(transfer_params_received)
+    if !@transfer_received.save
+      return render(json: @transfer_received.errors, status: :unprocessable_entity)
+    end
+      
+    Operations::TransferOperation.update_transfer_balance(@transfer_sent)
+    Operations::TransferOperation.update_transfer_balance(@transfer_received)
+    render(json: @transfer_sent, status: :created)
   end
 
   def update
     if @transfer.update(transfer_params)
-      render json: @transfer
+      render(json: @transfer)
     else
-      render json: @transfer.errors, status: :unprocessable_entity
+      render(json: @transfer.errors, status: :unprocessable_entity)
     end
   end
 
@@ -39,13 +45,24 @@ class TransfersController < ApplicationController
     end
 
     def transfer_params
-      params.require(:transfer).permit(:recipient_account, :balance)
+      params.require(:transfer).permit(:transfer_account, :balance)
     end
 
     def transfer_params_with_account
       transfer = transfer_params
       transfer["checking_account_id"] = id_checking_account
       return transfer
+    end
+
+    def transfer_params_received
+      @checking_account_received = CheckingAccount.find_by(account: @transfer_sent.transfer_account)
+      @checking_account_sent = CheckingAccount.find(@transfer_sent.checking_account_id)
+      return {
+        "balance": @transfer_sent.balance,
+        "status": 1,
+        "transfer_account": @checking_account_sent.account,
+        "checking_account_id": @checking_account_received.id
+      }
     end
 
     def transfer_status
