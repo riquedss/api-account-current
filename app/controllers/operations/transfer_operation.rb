929cc0137d3b2a34071efa863403fa29a31fad76
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-06 18:33:45 -0300

    feat: add class responsável com atualização do saldo nas transferência.

---
+++ app/controllers/operations/transfer_operation.rb
@@ -0,0 +1,30 @@
+module Operations
+  class TransferOperation
+    def self.update_transfer_balance(transfer)
+      if transfer.sent?
+        update_sender_balance(transfer)
+      else   
+        update_recipient_balance(transfer)
+      end
+    end
+
+    def self.update_sender_balance(transfer)
+      @checking_account = CheckingAccount.find(transfer.checking_account_id)
+      @user = User.find_by(id: @checking_account.user_id)
+      if @user.vip?
+        update_balance({ balance: @checking_account.balance - transfer.balance - (transfer.balance * 0.008) })
+      else
+        update_balance({ balance: @checking_account.balance - transfer.balance - 8 })
+      end
+    end
+
+    def self.update_recipient_balance(transfer)
+      @checking_account = CheckingAccount.find(transfer.checking_account_id)
+      update_balance({ balance: @checking_account.balance + transfer.balance })
+    end
+    
+    def self.update_balance(params)
+      @checking_account.update(params)
+    end
+  end
+end
