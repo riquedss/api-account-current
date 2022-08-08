b45ff3c7931d50f1880e2be0d277262ef84bec52
Henrique Santana <henrique.santana@injunior.com.br>
2022-08-08 17:27:44 -0300

    fix: linha comentada.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :name, :last_name, :email, :cpf, presence: true
   validates :name, :last_name, format: { with: REGEX_LETRA }
   validates :email, uniqueness: { case_sensitive: false }, format: { with: REGEX_EMAIL }
-  # validates :cpf, uniqueness: true, cpf: { mask: true }, format: { with: REGEX_CPF }
+  validates :cpf, uniqueness: true, cpf: { mask: true }, format: { with: REGEX_CPF }
   validates :password, length: { is: 6 }
 
   enum role: { comum: 0, vip: 1, manager: 2 }
